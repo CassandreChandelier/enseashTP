1c1f29a9f9ee83bf8bf628113fe39940347b5650
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2023-11-27 09:35:56 +0100

    added the main file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
 //
 // Created by cassa on 27/11/2023.
+#include <unistd.h> // for write
+#include <string.h> // for strlen
+
+int main() {
+    char welcomeMsg[] = "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\nenseash % ";
+    write(STDOUT_FILENO, welcomeMsg, strlen(welcomeMsg));
+
+    return 0;
+}
 //
