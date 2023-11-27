cecadc1da682afa7406cc89df9a30cfe23c6aaf6
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2023-11-27 10:31:22 +0100

    renamed files to match questions

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h> // for write
-#include <string.h> // for strlen
-
-int main() {
-    char welcomeMsg[] = "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\nenseash % ";
-    write(STDOUT_FILENO, welcomeMsg, strlen(welcomeMsg));
-
-    return 0;
-}
--- main3.c
+++ main3.c
@@ -1,8 +1,3 @@
-//
-// Created by cassa on 27/11/2023.
-#include <unistd.h> // for write
-#include <string.h> // for strlen
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,37 +7,42 @@
 
 int main() {
     char command[MAX_COMMAND_LENGTH];
-    char prompt[] = "enseash % ";
+    char prompt[] = "enseash % ";//what the user enters
 
     while (1) {
-        write(STDOUT_FILENO, prompt, strlen(prompt)); // Print the prompt
+        write(STDOUT_FILENO, prompt, strlen(prompt)); 
 
-       // Reading the command entered by the user
+        // Reading the command entered by the user
         ssize_t bytesRead = read(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
         if (bytesRead <= 0) {
-            write(STDOUT_FILENO, "\n", 1);  // Printing a newline and exit on empty input or error
+            write(STDOUT_FILENO, "\n", 1); // Printing a newline and exit on empty input or error
+            break;
+        }
+
+        // If the command is "exit" then terminate the loop
+        command[bytesRead - 1] = '\0'; // To remove newline character from the input
+        if (strcmp(command, "exit") == 0) {
+            write(STDOUT_FILENO, "Bye bye\n", 8);
             break;
         }
 
         // Executing the command
-        command[bytesRead - 1] = '\0'; // To remove newline character from the input
         int child_pid = fork();
 
         if (child_pid < 0) {
             write(STDOUT_FILENO, "Error forking process\n", 22);
             exit(EXIT_FAILURE);
         } else if (child_pid == 0) {
-            // Child process
-            execlp(command, command, NULL);  // Executing the command
-           // If execlp returns, a message error is displayed
+            // Child process :
+            execlp(command, command, NULL); // Executing the command
+            // If execlp returns, a message error is displayed
             write(STDOUT_FILENO, "Command not found\n", 18);
             exit(EXIT_FAILURE);
         } else {
-            // Parent process waits for child to finish (td)
+            // The parent process has to wait for the child to finish
             wait(NULL);
         }
     }
 
     return 0;
 }
-
