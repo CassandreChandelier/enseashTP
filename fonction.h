195eb070b44f90d994ba748999bb8ad800d916f8
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2023-11-27 11:44:51 +0100

    declared execute_command

--- fonction.h
+++ fonction.h
@@ -5,5 +5,5 @@
 #ifndef ENSEASHTP_FONCTION_H
 #define ENSEASHTP_FONCTION_H
 #define MAX_COMMAND_LENGTH 100
-void execute_command();
+void execute_command(char *buf, ssize_t, command_size);
 #endif //ENSEASHTP_FONCTION_H
